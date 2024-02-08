499a2b33f4be49d95a8a87371e305d906833739a
babayuuki <y.baba.hf@exv.co.jp>
2024-02-08 16:17:16 +0900

    base.html起用

--- menu_project/menu_project/settings.py
+++ menu_project/menu_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'menu_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'menu_app/templates/menu_app')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
